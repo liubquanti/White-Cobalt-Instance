ec6dc1895f963a64bd0272ae8324118fca5e54e7
Oleh Liubchenko <dev@liubquanti.click>
2026-03-21 09:26:05 +0100

    web/instance: to render

--- web/src/routes/version.json/+server.ts
+++ web/src/routes/version.json/+server.ts
@@ -1,12 +1,20 @@
 import { json } from "@sveltejs/kit";
 import { getCommit, getBranch, getRemote, getVersion } from "@imput/version-info";
 
+async function safeRead<T>(reader: () => Promise<T>, fallback: T): Promise<T> {
+    try {
+        return await reader();
+    } catch {
+        return fallback;
+    }
+}
+
 export async function GET() {
     return json({
-        commit: await getCommit(),
-        branch: await getBranch(),
-        remote: await getRemote(),
-        version: await getVersion()
+        commit: await safeRead(getCommit, null),
+        branch: await safeRead(getBranch, null),
+        remote: await safeRead(getRemote, null),
+        version: await safeRead(getVersion, "unknown")
     });
 }
 
